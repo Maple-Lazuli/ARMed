b60cca1581a12c72f0fd07a48cfcf0f033d60833
Ada <the.nostra.tymus@gmail.com>
2023-03-27 08:10:09 -0400

    updated the starter

--- start.py
+++ start.py
@@ -80,3 +80,65 @@ rules = association_rules(frequent_itemsets,
 
 # Print association rules
 print(rules)
+
+
+
+# heat map
+
+Snippets343
+Last Updated316
+# Replace frozen sets with strings
+rules['antecedents'] = rules['antecedents'].apply(lambda a: ','.join(list(a)))
+rules['consequents'] = rules['consequents'].apply(lambda a: ','.join(list(a)))
+
+# Transform data to matrix format and generate heatmap
+pivot = rules.pivot(index='consequents', columns='antecedents', values='support')
+sns.heatmap(pivot)
+
+# Format and display plot
+plt.yticks(rotation=0)
+plt.show()
+
+## parallel
+
+Snippets343
+Last Updated316
+￼Close
+￼Modify
+# Import seaborn under its standard alias
+import seaborn as sns
+
+# Apply the Apriori algorithm with a support value of 0.0075
+frequent_itemsets = apriori(onehot, min_support = 0.0075, 
+                         use_colnames = True, max_len = 2)
+
+# Generate association rules without performing additional pruning
+rules = association_rules(frequent_itemsets, metric = "support", 
+                          min_threshold = 0.0)
+
+# Generate scatterplot using support and confidence
+sns.scatterplot(x = "support", y = "confidence", 
+                size = "lift", data = rules)
+plt.show()
+
+
+## filterining
+
+Snippets343
+Last Updated316
+￼Close
+￼Modify
+# Import the association rules function
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# Compute frequent itemsets using the Apriori algorithm
+frequent_itemsets = apriori(onehot, min_support = 0.001, 
+                            max_len = 2, use_colnames = True)
+
+# Compute all association rules for frequent_itemsets
+rules = association_rules(frequent_itemsets, 
+                            metric = "lift", 
+                            min_threshold = 1)
+
+# Print association rules
+print(rules)
